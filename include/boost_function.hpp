e06177176346fc6fc0d6f3124649ec1f7115a191
Phil <CppPhil@users.noreply.github.com>
2018-08-14 16:10:38 +0200

    boost works now

---
+++ include/boost_function.hpp
@@ -0,0 +1,9 @@
+#ifndef INCG_CBP_BOOST_FUNCTION_HPP
+#define INCG_CBP_BOOST_FUNCTION_HPP
+
+namespace cbp
+{
+bool boost_function();
+} // namespace cbp
+#endif // INCG_CBP_BOOST_FUNCTION_HPP
+
---
+++ test/src/boost_function_test.cpp
@@ -0,0 +1,8 @@
+#include <doctest/doctest.h>
+#include "../../include/boost_function.hpp"
+
+TEST_CASE("boost_function_test")
+{
+	CHECK_UNARY(cbp::boost_function());
+}
+
